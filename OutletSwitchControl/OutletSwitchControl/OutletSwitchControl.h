35a3ce2c4b53c5f5f2c2681d21d0bff24e125ad1
gregpaton08 <gregpaton08@gmail.com>
2018-01-14 13:41:42 -0500

    Add framework for outlet controls

---
+++ OutletSwitchControl/OutletSwitchControl/OutletSwitchControl.h
@@ -0,0 +1,19 @@
+//
+//  OutletSwitchControl.h
+//  OutletSwitchControl
+//
+//  Created by Greg Paton on 1/14/18.
+//  Copyright © 2018 GSP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OutletSwitchControl.
+FOUNDATION_EXPORT double OutletSwitchControlVersionNumber;
+
+//! Project version string for OutletSwitchControl.
+FOUNDATION_EXPORT const unsigned char OutletSwitchControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OutletSwitchControl/PublicHeader.h>
+
+
